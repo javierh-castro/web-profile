69d7df543c05a6632fba5211a7e13e915f9dd3dc
javierh-castro <javi2cc1@gmail.com>
2024-08-28 14:47:24 -0300

    fix: Adding a new project

--- src/components/WorkProjects/WorkProjects.tsx
+++ src/components/WorkProjects/WorkProjects.tsx
@@ -1,12 +1,15 @@
 "use client"
 
+import { workprojects } from "../../services/langs/es.json";
 import useTranslation from "@/src/hooks/use-translation";
 import "./WorkProjects.css";
 import Image from "next/image";
 import { SiNextdotjs, SiTypescript, SiTailwindcss, SiReact, SiCss3, SiNodedotjs, SiZod } from "react-icons/si";
+// import
 
 export default function WorkProjects() {
-  const { t } = useTranslation();
+  const { t: translate } = useTranslation()
+  const t = (key: string) => translate(key as any);
 
   const redirectToExternalPage = (url) => {
     window.open(url, "_blank");
@@ -16,225 +19,49 @@ export default function WorkProjects() {
     <section className="work-projects">
       <h1>{t("nav.projects")}</h1>
       <ul className="projects">
-        <li className="project">
-          <Image
-            src="/img/buscarclima.png"
-            alt="Estado del tiempo"
-            width={500}
-            height={300}
-            onClick={() => redirectToExternalPage('https://cheery-cat-44c418.netlify.app/')}
-            className="cursor-pointer"
-          />
-          <div className="container-project">
-            <div className="description-project">
-              <h4 className="offer">{t("workprojects.title1")}</h4>
-              <p>
-                {t("workprojects.time")}
-              </p>
+          {workprojects.map((project, index) => (
+            <li className="project" key={project.title}>
+            <Image
+              src={`/img/${project.img}.png`}
+              alt="Estado del tiempo"
+              width={500}
+              height={300}
+              onClick={() => redirectToExternalPage(`${project.url}`)}
+              className="cursor-pointer"
+            />
+            <div className="container-project">
+              <div className="description-project">
+                <h4 className="offer">{t(`workprojects.${index}.title`)}</h4>
+                <p>
+                {t(`workprojects.${index}.description`)}
+                </p>
+              </div>
+              <ul className="url-links">
+                <li className="urls">
+                  <a
+                    href={`${project.github}`}
+                    target="_blank"
+                    className="bi bi-github"
+                  ></a>
+                  <a
+                    href={`${project.url}`}
+                    target="_blank"
+                    className="bi bi-link"
+                  ></a>
+                </li>
+                <li className="stack">
+                  {typeof project.skill === 'string' && project.skill.includes('Typescript') ?  <SiTypescript/> :  <></>}
+                  {typeof project.skill === 'string' && project.skill.includes('Next') ?  <SiNextdotjs/> :  <></>}
+                  {typeof project.skill === 'string' && project.skill.includes('Node') ?  <SiNodedotjs /> :  <></>}
+                  {typeof project.skill === 'string' && project.skill.includes('Zod') ?  <SiZod/> :  <></>}
+                  {typeof project.skill === 'string' && project.skill.includes('Tailwindcss') ?  <SiTailwindcss/> :  <></>}
+                  {typeof project.skill === 'string' && project.skill.includes('React') ?  <SiReact /> :  <></>}
+                  {typeof project.skill === 'string' && project.skill.includes('Css') ?  <SiCss3/> :  <></>}
+                </li>
+              </ul>
             </div>
-            <ul className="url-links">
-              <ul className="urls">
-                <a
-                  href="https://github.com/javierh-castro/clima-react"
-                  target="_blank"
-                  className="bi bi-github"
-                ></a>
-                <a
-                  href="https://cheery-cat-44c418.netlify.app/"
-                  target="_blank"
-                  className="bi bi-link"
-                ></a>
-              </ul>
-              <ul className="stack">
-                <SiNextdotjs/>
-                <SiTypescript/>
-                <SiZod/>
-              </ul>
-            </ul>
-          </div>
-        </li>
-        <li className="project">
-          <Image
-            src="/img/recipify.png"
-            alt="Recipify"
-            width={500}
-            height={300}
-            onClick={() => redirectToExternalPage('https://github.com/javierh-castro/proyect-recipify')}
-            className="cursor-pointer"
-          />
-          <div className="container-project">
-            <div className="description-project">
-              <h4 className="offer">Recipify</h4>
-              <p>
-              {t("workprojects.recipify", ["nextauth"])}
-              </p>
-            </div>
-            <ul className="url-links">
-              <ul className="urls">
-                <a
-                  href="https://github.com/javierh-castro/proyect-recipify"
-                  target="_blank"
-                  className="bi-github"
-                ></a>
-                <a
-                  href="https://github.com/javierh-castro/proyect-recipify"
-                  target="_blank"
-                  className="bi bi-link"
-                ></a>
-              </ul>
-              <ul className="stack">
-                <SiNextdotjs />
-                <SiTypescript />
-                <SiTailwindcss/>
-              </ul>
-            </ul>
-          </div>
-        </li>
-        <li className="project">
-          <Image
-            src="/img/commerce.png"
-            alt="E-commerce jc"
-            width={500}
-            height={300}
-            onClick={() => redirectToExternalPage('https://ecommerce-javier.netlify.app/')}
-            className="cursor-pointer"
-          />
-          <div className="container-project">
-            <div className="description-project">
-              <h4 className="offer">E-commerce web</h4>
-              <p>
-              {t("workprojects.ecommerce")}
-              </p>
-            </div>
-            <ul className="url-links">
-              <ul className="urls">
-                <a
-                  href="https://github.com/javierh-castro/proyect-ecommerce/tree/master"
-                  target="_blank"
-                  className="bi bi-github"
-                ></a>
-                <a
-                  href="https://ecommerce-javier.netlify.app/"
-                  target="_blank"
-                  className="bi bi-link"
-                ></a>
-              </ul>
-              <ul className="stack">
-                <SiReact />
-                <SiCss3 />
-              </ul>
-            </ul>
-          </div>
-        </li>
-
-        <li className="project">
-          <Image
-            src="/img/income expenses.png"
-            alt="Control financier"
-            width={500}
-            height={300}
-            onClick={() => redirectToExternalPage('https://income-expenses-example.netlify.app/')}
-            className="cursor-pointer"
-          />
-          <div className="container-project">
-            <div className="description-project">
-              <h4 className="offer">{t("workprojects.title4")}</h4>
-              <p>
-                {t("workprojects.financial")}
-              </p>
-            </div>
-            <ul className="url-links">
-              <ul className="urls">
-                <a
-                  href="https://github.com/javierh-castro/Income-and-Expenses"
-                  target="_blank"
-                  className="bi bi-github"
-                ></a>
-                <a
-                  href="https://income-expenses-example.netlify.app/"
-                  target="_blank"
-                  className="bi bi-link"
-                ></a>
-              </ul>
-              <ul className="stack">
-                <SiReact />
-                <SiTailwindcss />
-              </ul>
-            </ul>
-          </div>
-        </li>
-        <li className="project">
-          <Image
-            src="/img/tic tac toe.png"
-            alt="Tic Tac Toe"
-            width={500}
-            height={300}
-            onClick={() => redirectToExternalPage('https://javierh-castro.github.io/tic-tac-toe/')}
-            className="cursor-pointer"
-          />
-          <div className="container-project">
-            <div className="description-project">
-              <h4 className="offer">Tic Tac Toe</h4>
-              <p>
-                {t("workprojects.tictactoe")}
-              </p>
-            </div>
-            <ul className="url-links">
-              <ul className="urls">
-                <a
-                  href="https://github.com/javierh-castro/tic-tac-toe"
-                  target="_blank"
-                  className="bi bi-github"
-                ></a>
-                <a
-                  href="https://javierh-castro.github.io/tic-tac-toe/"
-                  target="_blank"
-                  className="bi bi-link"
-                ></a>
-              </ul>
-              <ul className="stack">
-                <SiReact />
-                <SiCss3 />
-              </ul>
-            </ul>
-          </div>
-        </li>
-        <li className="project">
-          <Image
-            src="/img/crud 2.png"
-            alt="Crud de usuarios"
-            width={500}
-            height={300}
-            onClick={() => redirectToExternalPage('https://github.com/javierh-castro/CRUD-2')}
-            className="cursor-pointer"
-          />
-          <div className="container-project">
-            <div className="description-project">
-              <h4 className="offer">Crud Users</h4>
-              <p>
-                {t("workprojects.crud")}
-              </p>
-            </div>
-            <ul className="url-links">
-              <ul className="urls">
-                <a
-                  href="https://github.com/javierh-castro/CRUD-2"
-                  target="_blank"
-                  className="bi bi-github"
-                ></a>
-                <a
-                  href="https://github.com/javierh-castro/CRUD-2"
-                  target="_blank"
-                  className="bi bi-link"
-                ></a>
-              </ul>
-              <ul className="stack">
-                <SiNodedotjs />
-                <SiTypescript />
-              </ul>
-            </ul>
-          </div>
-        </li>
+          </li>
+          ))}
       </ul>
     </section>
   );
