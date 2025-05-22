261d6238dd80692a74d02995fc7bf217568a7466
javierh-castro <javi2cc1@gmail.com>
2025-05-22 18:33:54 -0300

    fix: Corregir enlace del CV en el botón de descarga

--- src/components/Navbar/Button/ButtonCv.jsx
+++ src/components/Navbar/Button/ButtonCv.jsx
@@ -3,7 +3,7 @@ import "./Button.css";
 function ButtonCv() {
   return (
     <button class="button">
-      <a download="CV_JavierCastro" href="../../CV_JavierCastro.pdf">
+      <a download="CV_JavierCastro" href="../../CV Javier Castro.pdf">
         CV
       </a>
     </button>
